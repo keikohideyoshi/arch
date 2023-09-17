b2e948d18050afad57095d60fbd1a6019a33c81e
keikohideyoshi <drszigetib@gmail.com>
2023-09-17 21:25:10 +0200

    Added webapp-manager scripts and icons.

---
+++ hyprland/Scripts/chatgpt.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sh -c 'XAPP_FORCE_GTKWINDOW_ICON="org.kde.neochat" firefox --class WebApp-ChatGPT7843 --profile /home/drszigetib/.local/share/ice/firefox/ChatGPT7843 --no-remote "https://chat.openai.com"'
---
+++ hyprland/Scripts/facebook.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sh -c 'XAPP_FORCE_GTKWINDOW_ICON="facebook" firefox --class WebApp-Facebook7464 --profile /home/drszigetib/.local/share/ice/firefox/Facebook7464 --no-remote "https://www.facebook.com"'
