f64285d1c630a7773728bcbb7d9682e958b24452
keikohideyoshi <drszigetib@gmail.com>
2025-06-14 14:51:41 +0200

    New i3 and polybar config.

--- i3/.config/i3/mount.sh
+++ i3/.config/i3/mount.sh
@@ -2,4 +2,4 @@
 
 sudo mount /dev/sda1 /home/drszigetib/Mount/Keiko
 sudo mount /dev/sdb1 /home/drszigetib/Mount/Lanathyel/
-sudo mount /dev/sdc3 /home/drszigetib/Mount/Vyanda/
+#sudo mount /dev/sdc3 /home/drszigetib/Mount/Vyanda/
